708075f5c6deb6635a58c6ab858dec94e6f0a8b6
Ayush More <ayushmore27.am@gmail.com>
2026-07-26 12:55:51 +0530

    feat: implement TrainingFragment for interactive scam detection learning and XP tracking

--- android/app/src/main/java/com/aegisai/app/ui/training/TrainingFragment.kt
+++ android/app/src/main/java/com/aegisai/app/ui/training/TrainingFragment.kt
@@ -6,6 +6,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.launch
+import com.aegisai.app.data.SessionHelper
 import com.aegisai.app.databinding.FragmentTrainingBinding
 import androidx.navigation.fragment.findNavController
 import com.aegisai.app.R
